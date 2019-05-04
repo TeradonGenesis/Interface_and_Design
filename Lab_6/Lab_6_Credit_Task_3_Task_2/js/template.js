d0fbfdda3cd4eaae863a860901b0fad084507f8b
TeradonGenesis <poketrainereddie@gmail.com>
2019-05-04 21:51:18 +0800

    Added the second carousel

--- Lab_6/Lab_6_Credit_Task_3_Task_2/js/template.js
+++ Lab_6/Lab_6_Credit_Task_3_Task_2/js/template.js
@@ -8,62 +8,19 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope) {
     "use strict";
 
-    $scope.mainProduct1 = [{
-        Pic: 'images/main1.png',
-        Type: 'Kairos Series',
-        Price: '$149',
-        Colour: 23
-        }, {
-        Pic: 'images/main2.png',
-        Type: 'Bellwater Chrono',
-        Price: '$199',
-        Colour: 14
-        },
-                         {
-        Pic: 'images/main3.png',
-        Type: 'Marble Series',
-        Price: '$179',
-        Colour: 7
-        }];
-    
-    $scope.mainProduct2 = [{
-        Pic: 'images/main4.png',
-        Type: 'Kairos Series',
-        Price: '$149',
-        Colour: 23
-        },
-                         {
-        Pic: 'images/main5.png',
-        Type: 'Kairos Series',
-        Price: '$149',
-        Colour: 23
-        },
-                         {
-        Pic: 'images/main6.png',
-        Type: 'Kairos Series',
-        Price: '$149',
-        Colour: 23
-        }];
-    
-    $scope.mainProduct3 = [{
-        Pic: 'images/main6.png',
-        Type: 'Kairos Series',
-        Price: '$149',
-        Colour: 23
-        },
-                         {
-        Pic: 'images/main4.png',
-        Type: 'Kairos Series',
-        Price: '$149',
-        Colour: 23
-        },
-                         {
-        Pic: 'images/main3.png',
-        Type: 'Kairos Series',
-        Price: '$149',
-        Colour: 23
-        }];
-
+    $scope.logo = [{
+        pic: 'images/logo1.png'
+    }, {
+        pic: 'images/logo2.png'
+    }, {
+        pic: 'images/logo3.png'
+    }, {
+        pic: 'images/logo4.png'
+    }, {
+        pic: 'images/logo5.png'
+    }, {
+        pic: 'images/logo6.png'
+    }];
 
 });
 
@@ -74,13 +31,13 @@ app.directive('mainProduct1', function () {
             scope.pic = attributes.pic;
             scope.type = attributes.type;
             scope.price = attributes.price;
-            scope.colour = attributes.colour;// or attributes.ngDemo1;
+            scope.colour = attributes.colour; // or attributes.ngDemo1;
         };
 
     direc.restrict = 'EA';
     //E - Element, A - Attribute, C - Class, M - Comment
     direc.link = linkFunction;
-    direc.template = '<div class="col-xs-12 col-md-4"><center><a href="#"><img data-ng-src={{pic}} class="img-responsive"></a><br/><h4>{{type}}</h4>{{price}} | {{colur}} Color<br/><br/><button class="btn btn-light">View Collection</button></div></center>'; // text bind to the scope.text in linkFunction
+    direc.template = '<div class="col-xs-12 col-md-4"><center><a href="#"><img data-ng-src={{pic}} class="img-responsive"></a><br/><h4>{{type}}</h4>{{price}} | {{colour}} Color<br/><br/><button class="btn btn-light">View Collection</button></div></center>'; // text bind to the scope.text in linkFunction
     direc.scope = {}; //Special Setting to avoid instance's scope overring.
 
     return direc;
@@ -93,32 +50,69 @@ app.directive('mainProduct2', function () {
             scope.pic = attributes.pic;
             scope.type = attributes.type;
             scope.price = attributes.price;
-            scope.colour = attributes.colour;// or attributes.ngDemo1;
+            scope.colour = attributes.colour; // or attributes.ngDemo1;
         };
 
     direc.restrict = 'EA';
     //E - Element, A - Attribute, C - Class, M - Comment
     direc.link = linkFunction;
-    direc.template = '<div class="col-xs-12 col-md-4"><center><a href="#"><img data-ng-src={{pic}} class="img-responsive" ></a><br/><h4>{{type}}</h4>{{price}} | {{colur}} Color<br/><br/><button class="btn btn-light">View Collection</button></div></center>'; // text bind to the scope.text in linkFunction
+    direc.template = ' <div class="hidden-xs hidden-sm col-md-4"><center><a href="#"><img data-ng-src={{pic}} class="img-responsive"></a><br/><h4>{{type}}</h4>{{price}} | {{colour}} Color<br/><br/><button class="btn btn-light">View Collection</button></div></center>'; // text bind to the scope.text in linkFunction
     direc.scope = {}; //Special Setting to avoid instance's scope overring.
 
     return direc;
 });
 
-app.directive('mainProduct3', function () {
+app.directive('review1', function () {
     "use strict";
     var direc = {}, // Create an empty object
         linkFunction = function (scope, element, attributes) {
             scope.pic = attributes.pic;
-            scope.type = attributes.type;
-            scope.price = attributes.price;
-            scope.colour = attributes.colour;// or attributes.ngDemo1;
+            scope.rating = attributes.rating;
+            scope.purchase = attributes.purchase;
+            scope.comment = attributes.comment;
+            scope.reviewer = attributes.reviewer;
         };
 
     direc.restrict = 'EA';
     //E - Element, A - Attribute, C - Class, M - Comment
     direc.link = linkFunction;
-    direc.template = '<div class="col-xs-12 col-md-4"><center><a href="#"><img data-ng-src={{pic}} class="img-responsive"></a><br/><h4>{{type}}</h4>{{price}} | {{colur}} Color<br/><br/><button class="btn btn-light">View Collection</button></div></center>'; // text bind to the scope.text in linkFunction
+    direc.template = '<div class="col-xs-12 col-md-4 box"><div class="inner-box"><center><img data-ng-src={{pic}} class="watch-bought" /><br /><img src={{rating}} class="rating" /><br /><p>Purchased <a href=#>{{purchase}}</a></p><p>{{comment}}</p><br /><a href=#>Read More</a><br /><br /><p>-{{reviewer}}</p></center></div></div>'; // text bind to the scope.text in linkFunction
+    direc.scope = {}; //Special Setting to avoid instance's scope overring.
+
+    return direc;
+});
+
+app.directive('review2', function () {
+    "use strict";
+    var direc = {}, // Create an empty object
+        linkFunction = function (scope, element, attributes) {
+            scope.pic = attributes.pic;
+            scope.rating = attributes.rating;
+            scope.purchase = attributes.purchase;
+            scope.comment = attributes.comment;
+            scope.reviewer = attributes.reviewer;
+        };
+
+    direc.restrict = 'EA';
+    //E - Element, A - Attribute, C - Class, M - Comment
+    direc.link = linkFunction;
+    direc.template = '<div class="hidden-xs hidden-sm col-md-4 box"><div class="inner-box"><center><img data-ng-src={{pic}} class="watch-bought" /><br /><img src={{rating}} class="rating" /><br /><p>Purchased <a href=#>{{purchase}}</a></p><p>{{comment}}</p><br /><a href=#>Read More</a><br /><br /><p>-{{reviewer}}</p></center></div></div>'; // text bind to the scope.text in linkFunction
+    direc.scope = {}; //Special Setting to avoid instance's scope overring.
+
+    return direc;
+});
+
+app.directive('companyLogo', function () {
+    "use strict";
+    var direc = {}, // Create an empty object
+        linkFunction = function (scope, element, attributes) {
+            scope.pic = attributes.pic;
+        };
+
+    direc.restrict = 'EA';
+    //E - Element, A - Attribute, C - Class, M - Comment
+    direc.link = linkFunction;
+    direc.template = '<div class="col-xs-4 col-md-2 com"><img data-ng-src={{pic}} class="img-responsive" alt="rating" class="logo" /></div>'; // text bind to the scope.text in linkFunction
     direc.scope = {}; //Special Setting to avoid instance's scope overring.
 
     return direc;
